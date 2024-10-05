87878f75bff178c22ac83ef90520d76fb1bf146c
Hyeongrok Son <hyeson@c2r20s4.42gyeongsan.kr>
2024-10-05 15:11:48 +0900

    drift

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,24 +6,32 @@
 /*   By: hyeson <hyeson@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 16:50:52 by hyeson            #+#    #+#             */
-/*   Updated: 2024/10/05 12:54:40 by hyeson           ###   ########.fr       */
+/*   Updated: 2024/10/05 15:10:48 by hyeson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
+	t_list	*tmp;
 
 	new_lst = ft_lstnew(f(lst->content));
-	if (new_lst != NULL)
-	{
-		ft_lstclear(&new_lst, del);
+	if (new_lst == NULL)
 		return (NULL);
+	tmp = new_lst;
+	lst = lst->next;
+	while (lst != NULL)
+	{
+		tmp->next = ft_lstnew(f(lst->content));
+		if (tmp->next == NULL)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		tmp = tmp->next;
+		lst = lst->next;
 	}
-	if (lst->next)
-		new_lst->next = ft_lstmap(lst->next, f, del);
 	return (new_lst);
 }
