b1aecadf1b4d49bec71b9032954aad4d153e1ef4
Hyeongrok Son <hyeson@c2r6s7.42gyeongsan.kr>
2024-10-11 12:32:29 +0900

    grademe

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hyeson <hyeson@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 10:40:03 by hyeson            #+#    #+#             */
-/*   Updated: 2024/10/05 21:44:07 by hyeson           ###   ########.fr       */
+/*   Updated: 2024/10/10 13:48:40 by hyeson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if ((nmemb * size) / nmemb != size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
 	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
